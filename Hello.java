6d17cb32174153d84913823753c2e10521dc8525
Leonhdl <49850231+Leonhdl@users.noreply.github.com>
2019-05-14 13:59:00 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package com.zsyh;
+
+/**
+ * @Auther:YH
+ * @Date:2019/5/14
+ * @Description:com.zsyh
+ * @Version:1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
