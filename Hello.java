aa1429d5899c428daf332b1542e79686ee7eeee3
Leonhdl <49850231+Leonhdl@users.noreply.github.com>
2019-05-14 14:06:00 +0800

    优化代码
    一点点修改

--- Hello.java
+++ Hello.java
@@ -9,5 +9,6 @@ package com.zsyh;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world,hello github");
+		 System.out.println("hahaha");
     }
 }
